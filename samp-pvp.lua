30dfa2d09e7ef9c38f6ab27a4e37d3d0eb83d951
qrlk <hi@qrlk.me>
2019-09-24 20:55:52 +0300

    Removed inspect.lua require && minificated other code

--- samp-pvp.lua
+++ samp-pvp.lua
@@ -1,15 +1,1 @@
-local sampev = require 'lib.samp.events'
-function main()
-  if not isSampfuncsLoaded() or not isSampLoaded() then return end
-  while not isSampAvailable() do wait(100) end
-  wait(-1)
-end
-function sampev.onShowTextDraw(id, data)
-  if id == 0 then
-    i = require 'inspect'
-    data["text"] = "~b~~h~Samp-Pvp.Ru"
-    data["position"]["x"] = data["position"]["x"] 
-		data["position"]["y"] = data["position"]["y"] 
-    return {id, data}
-  end
-end
+s = require 'lib.samp.events' function s.onShowTextDraw(i, d) if i == 0 then d["text"] = "~b~~h~Samp-Pvp.Ru" return {i, d} end end
